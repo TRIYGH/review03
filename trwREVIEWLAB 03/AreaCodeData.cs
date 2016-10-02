927ab253b7357b8d262d6a46d630a9625afc6ad6
T. Robert Ward <trironyard@icloud.com>
2016-10-02 15:13:29 -0400

    initial commit

---
+++ trwREVIEWLAB 03/AreaCodeData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trwREVIEWLAB_03
+{
+    class AreaCodeData
+    {
+
+        public AreaCodeData(int [] arcodes)
+        {
+          int [] areaCodes = {910,919,828,336,704,980,984,252};
+
+            areaCodes[0] = 888;
+
+        }
+
+          /* public int AreaCodeData();
+            {
+                int [] areaCodes = {910,919,828,336,704,980,984,252};
+                return AreaCodeData;
+            } */
+}
